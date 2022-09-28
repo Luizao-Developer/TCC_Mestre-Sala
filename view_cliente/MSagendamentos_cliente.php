b31df5137f23b572d32dae11d0ba2be07c4ea553
Luiz <tteodoro1977@gmail.com>
2022-09-28 20:16:12 -0300

    (6) 28/09/2022

--- view_cliente/MSagendamentos_cliente.php
+++ view_cliente/MSagendamentos_cliente.php
@@ -2,6 +2,15 @@
 
 require_once("../controladorCliente/MSsessao_cliente.php");
 ?>
+<?php
+
+$conexao - mysqli_connect("127.0.0.1","root","","mestre_sala");
+
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -22,51 +31,17 @@ require_once("../controladorCliente/MSsessao_cliente.php");
 </div>
 <div class="pg_principal container">
 
-       <!--Form de realizar agendamento-->
-                    <form class="row g-3">
-                <div class="col-md-6">
-                    <label for="inputEmail4" class="form-label">Email</label>
-                    <input type="email" class="form-control" id="inputEmail4">
-                </div>
-                <div class="col-md-6">
-                    <label for="inputPassword4" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="inputPassword4">
-                </div>
-                <div class="col-12">
-                    <label for="inputAddress" class="form-label">Address</label>
-                    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-                </div>
-                <div class="col-12">
-                    <label for="inputAddress2" class="form-label">Address 2</label>
-                    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-                </div>
-                <div class="col-md-6">
-                    <label for="inputCity" class="form-label">City</label>
-                    <input type="text" class="form-control" id="inputCity">
-                </div>
-                <div class="col-md-4">
-                    <label for="inputState" class="form-label">State</label>
-                    <select id="inputState" class="form-select">
-                    <option selected>Choose...</option>
-                    <option>...</option>
-                    </select>
-                </div>
-                <div class="col-md-2">
-                    <label for="inputZip" class="form-label">Zip</label>
-                    <input type="text" class="form-control" id="inputZip">
-                </div>
-                <div class="col-12">
-                    <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="gridCheck">
-                    <label class="form-check-label" for="gridCheck">
-                        Check me out
-                    </label>
-                    </div>
-                </div>
-                <div class="col-12">
-                    <button type="submit" class="btn btn-primary">Sign in</button>
-                </div>
-                </form>
+       <!--Informações e realização de agendamento-->
+       <div class="alert alert-primary" role="alert">
+            <h4 class="alert-heading">Realizar agendamento</h4>
+            <p>Faça seu agendamento de forma rápida e ordenada</p>
+            <hr>
+            <p class="mb-0">
+                <a href="MSrealizarAgendamentos_cliente">
+            <button type="button" class="btn btn-primary"><i class="fas fa-cut"></i> Realizar agendamento</button>
+            </a>
+            </p>
+        </div>
         
 </div>
  
