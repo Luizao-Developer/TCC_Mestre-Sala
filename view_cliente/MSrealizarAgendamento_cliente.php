46692edf95f32a4244f743fc3d11fbd48e6f8390
Luiz <tteodoro1977@gmail.com>
2022-10-11 22:27:39 -0300

    (2) 11/10/2022

--- view_cliente/MSrealizarAgendamento_cliente.php
+++ view_cliente/MSrealizarAgendamento_cliente.php
@@ -74,7 +74,7 @@ tbBarbearia_Codigo-->
                 </div>
                 <div class="col-md-6">
                     <label for="inputEmail4" class="form-label">Procedimento</label>
-                    <select id="inputState" name="procedimento" class="form-select" required>
+                    <select id="inputState" name="procedimento" id="procedimento"class="form-select" required>
                     <?php while($listandoProcedimento = mysqli_fetch_array($consultando)): ?>
                     <option><?= $listandoProcedimento['Nome'] ?></option>
                     <?php endwhile ?>
@@ -118,14 +118,18 @@ tbBarbearia_Codigo-->
                 <div class="col-12">
                     <button type="submit" class="btn btn-primary" name="realiza_agendamento">Realizar agendamento</button>
                 </div>
+                
                 </form>
+               <div id="texto">
+regwerg
+               </div>
         
 </div>
- 
+<script src="../arquivos_jsCliente/MSrealizarAgendamento_cliente.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script>
-<script src="../arquivos_jsCliente/MSrealizarAgendamento_cliente.js"></script>
 <script>
+    
             // Example starter JavaScript for disabling form submissions if there are invalid fields
         (() => {
         'use strict'
@@ -145,6 +149,8 @@ tbBarbearia_Codigo-->
             }, false)
         })
         })()
+
+        
 </script>
 </body>
 
